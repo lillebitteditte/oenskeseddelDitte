cfc139ecbc1fecaa6619cf8c77390432e606bdbf
Isaac Levy <isaa0267@stud.kea.dk>
2023-04-11 13:41:03 +0200

    I make some comment in the home controller.

--- src/main/java/com/example/oenskeseddel/Model/Ønske.java
+++ src/main/java/com/example/oenskeseddel/Model/Ønske.java
@@ -1,37 +1,5 @@
 package com.example.oenskeseddel.Model;
 
 public class Ønske {
-    private String navn;
-    private String beskrivelse;
-    private String linke;
 
-    public Ønske(String navn, String beskrivelse, String linke) {
-        this.navn = navn;
-        this.beskrivelse = beskrivelse;
-        this.linke = linke;
-    }
-
-    public String getNavn() {
-        return navn;
-    }
-
-    public void setNavn(String navn) {
-        this.navn = navn;
-    }
-
-    public String getBeskrivelse() {
-        return beskrivelse;
-    }
-
-    public void setBeskrivelse(String beskrivelse) {
-        this.beskrivelse = beskrivelse;
-    }
-
-    public String getLinke() {
-        return linke;
-    }
-
-    public void setLinke(String linke) {
-        this.linke = linke;
-    }
 }
