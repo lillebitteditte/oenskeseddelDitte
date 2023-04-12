69f5dfbe37231e402bab5a74c76ad85959077654
weix0015 <weix0015@stud.kea.dk>
2023-04-12 13:26:45 +0200

    Add new login site and css

--- src/main/java/com/example/oenskeseddel/Controller/HomeController.java
+++ src/main/java/com/example/oenskeseddel/Controller/HomeController.java
@@ -2,6 +2,7 @@ package com.example.oenskeseddel.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -14,5 +15,10 @@ public class HomeController {
     @GetMapping("/createBruger")
     public String createBruger(){
         return "Home/createBruger";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "Home/login";
     }
 }
