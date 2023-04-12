2547d62373a7a347276b0be0391fb9e80193211b
Lasse Hallingskov <lass347s@stud.kea.dk>
2023-04-12 13:42:11 +0200

    Edited stylesheet.

--- src/main/java/com/example/oenskeseddel/Controller/HomeController.java
+++ src/main/java/com/example/oenskeseddel/Controller/HomeController.java
@@ -1,20 +1,31 @@
 package com.example.oenskeseddel.Controller;
 
+import com.example.oenskeseddel.Model.Bruger;
+import com.example.oenskeseddel.Service.BrugerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
+    BrugerService brugerService;
 
     @GetMapping("/")
     public String index() {
         return "Home/index";
     }
+
     // make methods in this line.
     @GetMapping("/createBruger")
-    public String createBruger(){
+    public String createBruger() {
         return "Home/createBruger";
+    }
+
+    @PostMapping("/createNewBruger")
+    public String createNew(@ModelAttribute Bruger bruger) {
+        brugerService.addBruger(bruger);
+        return "redirect:/";
     }
 
     @GetMapping("/login")
@@ -22,3 +33,4 @@ public class HomeController {
         return "Home/login";
     }
 }
+
