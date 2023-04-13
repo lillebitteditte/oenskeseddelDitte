85be053f507e1a0f2d92b1c5f0415ec469df50e5
Lasse Hallingskov <lass347s@stud.kea.dk>
2023-04-13 14:57:51 +0200

    We have a problem in the HomeController.

--- src/main/java/com/example/oenskeseddel/Controller/HomeController.java
+++ src/main/java/com/example/oenskeseddel/Controller/HomeController.java
@@ -2,20 +2,27 @@ package com.example.oenskeseddel.Controller;
 
 import com.example.oenskeseddel.Model.Bruger;
 import com.example.oenskeseddel.Model.Login;
+import com.example.oenskeseddel.Model.Ønske;
 import com.example.oenskeseddel.Service.BrugerService;
 import com.example.oenskeseddel.Service.LoginService;
+import com.example.oenskeseddel.Service.ØnskeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
     private BrugerService brugerService;
     private LoginService loginService;
-    public HomeController(BrugerService brugerService, LoginService loginService) {
+    private ØnskeService ønskeService;
+    public HomeController(BrugerService brugerService, LoginService loginService,ØnskeService ønskeService) {
         this.brugerService = brugerService;
         this.loginService=loginService;
+        this.ønskeService=ønskeService;
     }
 
 
@@ -30,11 +37,24 @@ public class HomeController {
     public String createBruger() {
         return "Home/createBruger";
     }
+    @GetMapping("/createOenskeseddel")
+    public String index(Model model) {
+        List<Ønske>ønskeList= ønskeService.fetchAll();
+        model.addAttribute("oensker",ønskeList);
+        return "redirect:Home/createOenskeseddel";
+    }
+
+    @PostMapping("/createNewOenske")
+    public String createNewØnske(@ModelAttribute Ønske ønske){
+    ønskeService.addØnske(ønske);
+    return "redirect:Home/createOenskeseddel";
+    }
 
 
     // når oplysninger er indtastet
     @PostMapping("/createNewBruger")
     public String createNew(@ModelAttribute Bruger bruger) {
+
         brugerService.addBruger(bruger);
         return "redirect:/";
     }
